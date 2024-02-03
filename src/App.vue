53dcbf94fdc369913bbf09cf77cd17800fa60499
Maksym Siroshtan <siroshtan.max1991ua@gmail.com>
2024-02-03 13:02:43 +0200

    Filters added. Request to server using axios.

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,45 @@
 <script setup>
-import { onMounted, ref } from 'vue'
+import axios from 'axios'
+import { onMounted, reactive, ref, watch } from 'vue'
 
 import Header from './components/Header.vue'
 import CardList from './components/CardList.vue'
 
 const items = ref([])
-
-onMounted(() => {
-  fetch('https://847f8446d0bdc264.mokky.dev/items')
-    .then((res) => res.json())
-    .then((data) => {
-      items.value = data
-    })
+const filters = reactive({
+  sortBy: 'title',
+  searchQuery: ''
 })
+
+const onChangeSelect = (event) => {
+  filters.sortBy = event.target.value
+}
+
+const onChangeSearchInput = (event) => {
+  filters.searchQuery = event.target.value
+}
+
+const fetchItems = async () => {
+  try {
+    const params = {
+      sortBy: filters.sortBy
+    }
+
+    if (filters.searchQuery) {
+      params.title = `*${filters.searchQuery}*`
+    }
+
+    const { data } = await axios.get('https://847f8446d0bdc264.mokky.dev/items', {
+      params
+    })
+    items.value = data
+  } catch (err) {
+    console.log(err)
+  }
+}
+
+onMounted(fetchItems)
+watch(filters, fetchItems)
 </script>
 
 <template>
@@ -20,7 +47,25 @@ onMounted(() => {
     <Header />
 
     <div class="p-12">
-      <h2 class="text-3xl font-bold mb-12">Вся кава</h2>
+      <div class="flex items-center justify-between mb-12">
+        <h2 class="text-3xl font-bold">Наш асортимент кави</h2>
+        <div class="flex gap-8">
+          <select @change="onChangeSelect" class="border outline-none px-4 py-2 rounded-lg">
+            <option value="title">За назвою</option>
+            <option value="price">За ціною (зростання)</option>
+            <option value="-price">За ціною (зменшення)</option>
+          </select>
+          <div class="relative">
+            <img src="/search.svg" alt="Search" class="absolute top-3 left-4" />
+            <input
+              @input="onChangeSearchInput"
+              type="text"
+              placeholder="Пошук..."
+              class="border rounded-lg pl-10 pr-4 py-2 outline-none focus:border-slate-500"
+            />
+          </div>
+        </div>
+      </div>
 
       <CardList :items="items" />
     </div>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,7 +1,7 @@
 <template>
-  <header class="flex justify-between items-center border-b border-slate-200 px-6 py-4">
+  <header class="flex justify-between items-center border-b border-slate-200 pl-8 pr-12 py-4">
     <div class="flex items-center gap-3">
-      <img src="/logo.png" alt="Logo" class="w-28" />
+      <img src="/logo.png" alt="Logo" class="w-24" />
       <div>
         <h2 class="text-3xl uppercase font-bold">coffee shop</h2>
         <p class="text-xl text-slate-400">Магазин найсмачнішої кави</p>
